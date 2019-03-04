0898e235ea008b984296517570f77cf0c02c0bb7
aaargha <34s68mr5@ex456gcde6.com>
2019-03-04 20:20:42 +0100

    Update for 0.17

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Screenshot-Camera
 Factorio mod that adds a camera for taking screenshots
 
-Download [here](https://mods.factorio.com/mods/aaargha/SC_camera/)
+Download [here](https://mods.factorio.com/mod/SC_camera)
 
 More info [here](https://forums.factorio.com/viewtopic.php?f=92&t=42738)
--- prototypes/item.lua
+++ prototypes/item.lua
@@ -5,13 +5,12 @@ data:extend({
     name = "ScreenshotCamera",
     icon = "__SC_camera__/graphics/icon_camera.png",
 	icon_size = 32,
-    flags = { "goes-to-quickbar" },
     subgroup = "tool",
     stack_size= 1,
-	selection_mode = {"tiles"},
+	selection_mode = {"nothing"},
 	selection_cursor_box_type = "pair",
 	selection_color = {r = 0.7, g = 0, b = 0.7},
-	alt_selection_mode = {"tiles"},
+	alt_selection_mode = {"nothing"},
 	alt_selection_color = {r = 0.7, g = 7, b = 0},
 	alt_selection_cursor_box_type = "pair"
   }
